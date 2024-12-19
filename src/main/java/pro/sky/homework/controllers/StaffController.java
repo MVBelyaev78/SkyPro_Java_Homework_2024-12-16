5e72cebf80e5e93bdbc95c037512b810a4b29bd2
Mikhail Belyaev <mbelyaev78@gmail.com>
2024-12-20 00:57:13 +0300

    injecting "EmployeeService"

--- src/main/java/pro/sky/homework/controllers/StaffController.java
+++ src/main/java/pro/sky/homework/controllers/StaffController.java
@@ -2,8 +2,14 @@ package pro.sky.homework.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.homework.services.EmployeeService;
 
 @RestController
 @RequestMapping("/employees")
 public class StaffController {
+    private final EmployeeService employeeService;
+
+    public StaffController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 }
