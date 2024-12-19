724f853d1a8e198edffb1c1d64ab30c771a1562e
Mikhail Belyaev <mbelyaev78@gmail.com>
2024-12-19 21:50:48 +0300

    first Spring Boot commit

---
+++ src/main/java/pro/sky/SkyPro_Java_Homework_2024_12_16/SkyProJavaHomework20241216Application.java
@@ -0,0 +1,13 @@
+package pro.sky.SkyPro_Java_Homework_2024_12_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProJavaHomework20241216Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProJavaHomework20241216Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/SkyPro_Java_Homework_2024_12_16/SkyProJavaHomework20241216ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.SkyPro_Java_Homework_2024_12_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProJavaHomework20241216ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
