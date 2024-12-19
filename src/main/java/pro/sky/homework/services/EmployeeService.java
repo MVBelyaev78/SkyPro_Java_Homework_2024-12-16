1f35b90999bda02a4930adf0a422b6aafba7bbc7
Mikhail Belyaev <mbelyaev78@gmail.com>
2024-12-20 00:54:43 +0300

    stereotype Service in "EmployeeService"

--- src/main/java/pro/sky/homework/services/EmployeeService.java
+++ src/main/java/pro/sky/homework/services/EmployeeService.java
@@ -1,9 +1,11 @@
 package pro.sky.homework.services;
 
+import org.springframework.stereotype.Service;
 import pro.sky.homework.components.Employee;
 
 import java.util.List;
 
+@Service
 public interface EmployeeService {
     List<Employee> getEmployeeList();
 }
