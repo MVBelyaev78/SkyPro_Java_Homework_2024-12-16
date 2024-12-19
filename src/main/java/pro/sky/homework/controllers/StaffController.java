c4694bb43e66fb186b42cb4904d12fad6a0315d7
Mikhail Belyaev <mbelyaev78@gmail.com>
2024-12-20 01:13:48 +0300

    injecing added feature

--- src/main/java/pro/sky/homework/controllers/StaffController.java
+++ src/main/java/pro/sky/homework/controllers/StaffController.java
@@ -1,7 +1,10 @@
 package pro.sky.homework.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.homework.components.Employee;
 import pro.sky.homework.services.EmployeeService;
 
 @RestController
@@ -11,5 +14,10 @@ public class StaffController {
 
     public StaffController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public Employee getEmployeeById(@PathVariable Integer id) {
+        return employeeService.getEmployeeById(id);
     }
 }
