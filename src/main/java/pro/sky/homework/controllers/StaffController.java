12b393759e07ddcffb1dc3684e24ee27390a4170
Mikhail Belyaev <mbelyaev78@gmail.com>
2024-12-20 02:28:19 +0300

    piece controllers together

--- src/main/java/pro/sky/homework/controllers/StaffController.java
+++ src/main/java/pro/sky/homework/controllers/StaffController.java
@@ -5,18 +5,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.homework.components.Department;
+import pro.sky.homework.components.Employee;
 import pro.sky.homework.services.DepartmentService;
+import pro.sky.homework.services.EmployeeService;
 
 @RestController
-@RequestMapping("/departments")
-public class DepartmentController {
+@RequestMapping("")
+public class StaffController {
+    private final EmployeeService employeeService;
     private final DepartmentService departmentService;
 
-    public DepartmentController(DepartmentService departmentService) {
+    public StaffController(EmployeeService employeeService, DepartmentService departmentService) {
+        this.employeeService = employeeService;
         this.departmentService = departmentService;
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/employee/{id}")
+    public Employee getEmployeeById(@PathVariable Integer id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @GetMapping(value = "/department/{id}")
     public Department getDepartmentById(@PathVariable Integer id) {
         return departmentService.getDepartmentById(id);
     }
