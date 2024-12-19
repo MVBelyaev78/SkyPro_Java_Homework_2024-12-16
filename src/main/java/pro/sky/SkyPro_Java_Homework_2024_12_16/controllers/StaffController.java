356cf0d94a82f81670f7e3bdfd710300c8ee82d3
Mikhail Belyaev <mbelyaev78@gmail.com>
2024-12-20 00:19:51 +0300

    adding empty controller

---
+++ src/main/java/pro/sky/SkyPro_Java_Homework_2024_12_16/controllers/StaffController.java
@@ -0,0 +1,9 @@
+package pro.sky.SkyPro_Java_Homework_2024_12_16.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class StaffController {
+}
