6434a11c78e254ae437e344e6d88b939958fffc7
Mikhail Belyaev <mbelyaev78@gmail.com>
2024-12-20 02:15:55 +0300

    splitting controller between Employee and Department services

--- src/main/java/pro/sky/homework/controllers/DepartmentController.java
+++ src/main/java/pro/sky/homework/controllers/DepartmentController.java
@@ -5,27 +5,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sky.homework.components.Department;
-import pro.sky.homework.components.Employee;
 import pro.sky.homework.services.DepartmentService;
-import pro.sky.homework.services.EmployeeService;
 
 @RestController
-@RequestMapping("")
-public class StaffController {
-    private final EmployeeService employeeService;
+@RequestMapping("/departments")
+public class DepartmentController {
     private final DepartmentService departmentService;
 
-    public StaffController(EmployeeService employeeService, DepartmentService departmentService) {
-        this.employeeService = employeeService;
+    public DepartmentController(DepartmentService departmentService) {
         this.departmentService = departmentService;
     }
 
-    @GetMapping(value = "/employees/{id}")
-    public Employee getEmployeeById(@PathVariable Integer id) {
-        return employeeService.getEmployeeById(id);
-    }
-
-    @GetMapping(value = "/departments/{id}")
+    @GetMapping(value = "/{id}")
     public Department getDepartmentById(@PathVariable Integer id) {
         return departmentService.getDepartmentById(id);
     }
