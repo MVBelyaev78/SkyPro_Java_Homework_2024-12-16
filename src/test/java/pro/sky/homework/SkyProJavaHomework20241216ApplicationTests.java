b709950c740f35e38dc26a1f2b24b09f256bcba3
Mikhail Belyaev <mbelyaev78@gmail.com>
2024-12-20 13:39:28 +0300

    tsts initialized

--- src/test/java/pro/sky/homework/SkyProJavaHomework20241216ApplicationTests.java
+++ src/test/java/pro/sky/homework/SkyProJavaHomework20241216ApplicationTests.java
@@ -2,12 +2,19 @@ package pro.sky.homework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import pro.sky.homework.repositories.StaffRepository;
+import pro.sky.homework.services.DepartmentService;
+import pro.sky.homework.services.DepartmentServiceImpl;
+import pro.sky.homework.services.EmployeeService;
+import pro.sky.homework.services.EmployeeServiceImpl;
 
 @SpringBootTest
 class SkyProJavaHomework20241216ApplicationTests {
+    EmployeeService employeeService = new EmployeeServiceImpl(StaffRepository.getEmployees());
+    DepartmentService departmentService = new DepartmentServiceImpl(StaffRepository.getDepartments());
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
